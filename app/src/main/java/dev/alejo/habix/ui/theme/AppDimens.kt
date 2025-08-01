6414590b54da74daec3a7e01b8d5c08a2a17d778
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-01 18:53:49 -0500

    feat: add more dimension values

--- app/src/main/java/dev/alejo/habix/ui/theme/AppDimens.kt
+++ app/src/main/java/dev/alejo/habix/ui/theme/AppDimens.kt
@@ -4,7 +4,10 @@ import androidx.compose.ui.unit.dp
 
 object AppDimens {
     val ExtraTiny = 2.dp
+    val Tiny = 6.dp
     val Small = 8.dp
+    val Medium = 12.dp
     val Default = 16.dp
     val Large = 32.dp
+    val ExtraLarge = 64.dp
 }
