c3dc28518179283a2e508a5c93970f2bbc1fddc5
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-01 12:21:10 -0500

    feat: add AppDimens object

---
+++ app/src/main/java/dev/alejo/habix/ui/theme/AppDimens.kt
@@ -0,0 +1,10 @@
+package dev.alejo.habix.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object AppDimens {
+    val ExtraTiny = 2.dp
+    val Small = 8.dp
+    val Default = 16.dp
+    val Large = 32.dp
+}
