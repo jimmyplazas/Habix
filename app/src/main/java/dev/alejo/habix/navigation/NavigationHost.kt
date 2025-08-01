fadd158d1bea4ceafd8b37547909a5f048ddb0d8
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-01 18:54:32 -0500

    feat: implement LoginScreen in navigation host

--- app/src/main/java/dev/alejo/habix/navigation/NavigationHost.kt
+++ app/src/main/java/dev/alejo/habix/navigation/NavigationHost.kt
@@ -1,12 +1,12 @@
 package dev.alejo.habix.navigation
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.entry
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
+import dev.alejo.habix.authentication.presentation.login.LoginScreen
 import dev.alejo.habix.onboarding.presentation.OnboardingScreen
 
 @Composable
@@ -26,7 +26,7 @@ fun NavigationHost(
                 }
             }
             entry<NavigationScreens.Login> {
-                Text("Login")
+                LoginScreen()
             }
         }
     )
