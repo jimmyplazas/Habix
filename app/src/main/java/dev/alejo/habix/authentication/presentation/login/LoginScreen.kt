47a17b74630384647bf08d150f9a1185962278a3
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-01 18:55:30 -0500

    feat: create LoginScreen

---
+++ app/src/main/java/dev/alejo/habix/authentication/presentation/login/LoginScreen.kt
@@ -0,0 +1,36 @@
+package dev.alejo.habix.authentication.presentation.login
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.alejo.habix.authentication.presentation.login.components.LoginBackground
+import dev.alejo.habix.authentication.presentation.login.components.LoginForm
+import dev.alejo.habix.core.presentation.HabixTitle
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        LoginBackground()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.weight(3f))
+            HabixTitle("Welcome to")
+            HabixTitle("monumental habits")
+            Spacer(modifier = Modifier.weight(1f))
+            LoginForm()
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun LoginScreenPreview() {
+    LoginScreen()
+}
