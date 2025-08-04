e582fc809657134163cf728b50c6142282b8a1da
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-04 11:54:10 -0500

    feat: add sign up screen to navigation

--- app/src/main/java/dev/alejo/habix/navigation/NavigationScreens.kt
+++ app/src/main/java/dev/alejo/habix/navigation/NavigationScreens.kt
@@ -9,4 +9,6 @@ sealed class NavigationScreens: NavKey {
     data object Onboarding : NavigationScreens()
     @Serializable
     data object Login : NavigationScreens()
+    @Serializable
+    data object SignUp : NavigationScreens()
 }
