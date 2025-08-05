4eda2c9ba055c10ee0e42d612648d113289552a7
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-04 20:11:37 -0500

    feat: integrate ViewModel with SignUpScreen

--- app/src/main/java/dev/alejo/habix/authentication/presentation/signup/SignUpScreen.kt
+++ app/src/main/java/dev/alejo/habix/authentication/presentation/signup/SignUpScreen.kt
@@ -7,11 +7,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import dev.alejo.habix.R
 import dev.alejo.habix.authentication.presentation.signup.components.SignUpForm
 import dev.alejo.habix.core.presentation.HabixTitle
@@ -19,11 +23,24 @@ import dev.alejo.habix.core.presentation.HabixTitle
 @Composable
 fun SignUpScreen(
     modifier: Modifier = Modifier,
-    onSignUpClick: () -> Unit,
-    onLoginClick: () -> Unit
+    viewModel: SignUpViewModel = hiltViewModel(),
+    navigateToHome: () -> Unit,
+    navigateToLogin: () -> Unit
 ) {
+    val state by viewModel.state.collectAsState()
+
+    LaunchedEffect(viewModel) {
+        viewModel.effect.collect { effect ->
+            when (effect) {
+                is SignUpEffect.NavigateToLogin -> navigateToLogin()
+                is SignUpEffect.NavigateToHome -> navigateToHome()
+            }
+        }
+    }
+
     Column(
-        modifier = modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
+        modifier = modifier.fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
@@ -33,11 +50,7 @@ fun SignUpScreen(
             modifier = Modifier.scale(1f)
         )
         HabixTitle("Create your account")
-        SignUpForm(
-            onSignUpClick = onSignUpClick,
-            onLoginClick = onLoginClick
-        )
-
+        SignUpForm(state = state, onEvent = viewModel::onEvent)
     }
 }
 
@@ -45,7 +58,7 @@ fun SignUpScreen(
 @Composable
 private fun SignUpScreenPreview() {
     SignUpScreen(
-        onSignUpClick = {},
-        onLoginClick = {}
+        navigateToHome = {},
+        navigateToLogin = {}
     )
 }
