605c7c18193037f157f25cffcd8e9c774466e5ca
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-04 20:09:53 -0500

    feat: implement login form with email and password fields

--- app/src/main/java/dev/alejo/habix/authentication/presentation/login/components/LoginForm.kt
+++ app/src/main/java/dev/alejo/habix/authentication/presentation/login/components/LoginForm.kt
@@ -2,10 +2,16 @@ package dev.alejo.habix.authentication.presentation.login.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,55 +19,130 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import dev.alejo.habix.authentication.core.presentation.AuthTextFields
+import dev.alejo.habix.authentication.presentation.login.LoginEvent
+import dev.alejo.habix.authentication.presentation.login.LoginState
 import dev.alejo.habix.core.presentation.HabixButton
+import dev.alejo.habix.core.presentation.HabixPasswordTextField
+import dev.alejo.habix.core.presentation.HabixTextField
 import dev.alejo.habix.ui.theme.AppDimens
 
 @Composable
 fun LoginForm(
-    modifier: Modifier = Modifier,
-    onLoginClick: () -> Unit = {},
-    onSignUpClick: () -> Unit = {}
+    state: LoginState,
+    onEvent: (LoginEvent) -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier.background(
-            color = Color.White,
-            shape = RoundedCornerShape(topStart = AppDimens.Large, topEnd = AppDimens.Large)
-        ),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(AppDimens.Default)
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            "Login with Email",
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.tertiary,
-            modifier = Modifier.padding(top = AppDimens.Default).padding(vertical = AppDimens.Tiny)
-        )
-        HorizontalDivider(
-            modifier = Modifier
-                .fillMaxWidth(),
-            color = MaterialTheme.colorScheme.background
-        )
+        Column(
+            modifier = Modifier.background(
+                color = Color.White,
+                shape = RoundedCornerShape(topStart = AppDimens.Large, topEnd = AppDimens.Large)
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(AppDimens.Default)
+        ) {
+            Text(
+                "Login with Email",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.tertiary,
+                modifier = Modifier
+                    .padding(top = AppDimens.Default)
+                    .padding(vertical = AppDimens.Tiny)
+            )
+            HorizontalDivider(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                color = MaterialTheme.colorScheme.background
+            )
 
-        AuthTextFields()
+            LoginTextFields(state = state, onEvent = onEvent)
 
-        HabixButton(
-            text = "Login",
+            HabixButton(
+                text = "Login",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = AppDimens.Default),
+                isEnabled = !state.isLoading
+            ) { onEvent(LoginEvent.Login) }
+
+            LoginOptions { onEvent(LoginEvent.SignUp) }
+        }
+
+        if (state.isLoading) {
+            CircularProgressIndicator()
+        }
+    }
+}
+
+@Composable
+fun LoginTextFields(
+    state: LoginState,
+    onEvent: (LoginEvent) -> Unit
+) {
+    val focusManager = LocalFocusManager.current
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HabixTextField(
+            value = state.email,
+            onValueChange = { onEvent(LoginEvent.EmailChange(it)) },
+            placeholder = "Email",
+            contentDescription = "Enter email",
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(bottom = AppDimens.Tiny)
                 .padding(horizontal = AppDimens.Default),
-            isEnabled = true
-        ) { onLoginClick() }
+            leadingIcon = Icons.Outlined.Email,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onAny = {
+                    focusManager.moveFocus(FocusDirection.Next)
+                }
+            ),
+            isEnabled = !state.isLoading,
+            errorMessage = state.emailError
+        )
 
-        LoginOptions { onSignUpClick() }
-
+        HabixPasswordTextField(
+            value = state.password,
+            onValueChange = { onEvent(LoginEvent.PasswordChange(it)) },
+            placeholder = "Password",
+            contentDescription = "Enter Password",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = AppDimens.Tiny)
+                .padding(horizontal = AppDimens.Default),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onAny = {
+                    focusManager.clearFocus()
+                    onEvent(LoginEvent.Login)
+                }
+            ),
+            isEnabled = !state.isLoading,
+            errorMessage = state.passwordError
+        )
     }
 }
 
@@ -100,5 +181,11 @@ fun LoginOptions(onSignUpClick: () -> Unit) {
 @Preview(showBackground = true)
 @Composable
 private fun LoginFormPreview() {
-    LoginForm(Modifier.background(Color.Black).padding(AppDimens.Default))
+    LoginForm(
+        state = LoginState(),
+        onEvent = {},
+        modifier = Modifier
+            .background(Color.Black)
+            .padding(AppDimens.Default)
+    )
 }
