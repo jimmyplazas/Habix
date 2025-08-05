5b472d8c8de85f208503a3bad2410820f9ea27d8
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-04 20:10:32 -0500

    feat: add authentication check to MainViewModel

--- app/src/main/java/dev/alejo/habix/MainViewModel.kt
+++ app/src/main/java/dev/alejo/habix/MainViewModel.kt
@@ -5,13 +5,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.alejo.habix.authentication.domain.usecase.GetUserIdUseCase
 import dev.alejo.habix.onboarding.domain.usecase.HasSeenOnboardingUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val hasSeenOnboardingUseCase: HasSeenOnboardingUseCase
+    private val hasSeenOnboardingUseCase: HasSeenOnboardingUseCase,
+    private val getUSerIdUseCase: GetUserIdUseCase
 ) : ViewModel() {
+
     var hasSeenOnboarding by mutableStateOf(hasSeenOnboardingUseCase())
         private set
+    var isLoggedIn by mutableStateOf(getUSerIdUseCase() != null)
+        private set
+
 }
