92b868a7cb18674bd51a1583eff39304fa0c26ff
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-06 20:32:49 -0500

    feat: add edit and add habit events

--- app/src/main/java/dev/alejo/habix/habits/presentation/home/HomeEvent.kt
+++ app/src/main/java/dev/alejo/habix/habits/presentation/home/HomeEvent.kt
@@ -4,6 +4,8 @@ import dev.alejo.habix.habits.domain.model.Habit
 import java.time.ZonedDateTime
 
 sealed class HomeEvent {
-    data class GangeDate(val date: ZonedDateTime) : HomeEvent()
+    data class ChangeDate(val date: ZonedDateTime) : HomeEvent()
     data class CompleteHabit(val habit: Habit) : HomeEvent()
+    data class EditHabit(val habitId: String) : HomeEvent()
+    data object AddHabit : HomeEvent()
 }
