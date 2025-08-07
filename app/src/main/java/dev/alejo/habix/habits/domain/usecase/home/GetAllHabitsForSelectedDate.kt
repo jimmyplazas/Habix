89dffda2e3b8f32433e7fc5ec82290b710368829
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-06 20:31:59 -0500

    refactor: organize use cases by feature

--- app/src/main/java/dev/alejo/habix/habits/domain/usecase/home/GetAllHabitsForSelectedDate.kt
+++ app/src/main/java/dev/alejo/habix/habits/domain/usecase/home/GetAllHabitsForSelectedDate.kt
@@ -1,12 +1,12 @@
-package dev.alejo.habix.habits.domain.usecase
+package dev.alejo.habix.habits.domain.usecase.home
 
 import dev.alejo.habix.habits.domain.model.Habit
-import dev.alejo.habix.habits.domain.repository.HomeRepository
+import dev.alejo.habix.habits.domain.repository.HabitRepository
 import kotlinx.coroutines.flow.Flow
 import java.time.ZonedDateTime
 
 class GetAllHabitsForSelectedDate(
-    private val repository: HomeRepository
+    private val repository: HabitRepository
 ) {
     operator fun invoke(date: ZonedDateTime): Flow<List<Habit>> {
         return repository.getAllHabitsForSelectedDate(date)
