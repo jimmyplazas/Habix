9091070ccd284263892875f9dadbc02bef6be733
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-08 19:12:24 -0500

    feat: execute CompleteHabitUseCase on IO dispatcher

--- app/src/main/java/dev/alejo/habix/habits/domain/usecase/home/CompleteHabitUseCase.kt
+++ app/src/main/java/dev/alejo/habix/habits/domain/usecase/home/CompleteHabitUseCase.kt
@@ -2,12 +2,14 @@ package dev.alejo.habix.habits.domain.usecase.home
 
 import dev.alejo.habix.habits.domain.model.Habit
 import dev.alejo.habix.habits.domain.repository.HabitRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.time.ZonedDateTime
 
 class CompleteHabitUseCase (
     private val repository: HabitRepository
 ) {
-    suspend operator fun invoke(habit: Habit, date: ZonedDateTime) {
+    suspend operator fun invoke(habit: Habit, date: ZonedDateTime) = withContext(Dispatchers.IO) {
         val newHabit = if (habit.completedDates.contains(date.toLocalDate())) {
             habit.copy(completedDates = habit.completedDates - date.toLocalDate())
         } else {
