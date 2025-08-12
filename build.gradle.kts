bbb71d078969805f0a4175c039389ab704548496
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-11 21:18:33 -0500

    Refactor: replace KSP with Kapt

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,5 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.crashlytics) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.ksp) apply false
+    //alias(libs.plugins.ksp) apply false
 }
