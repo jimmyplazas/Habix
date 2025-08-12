e5d671b04cefad06745b4b918c570f00c487711d
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-11 21:20:14 -0500

    feat: add `HabitSyncEntity` to HomeDatabase

--- app/src/main/java/dev/alejo/habix/habits/data/local/HomeDatabase.kt
+++ app/src/main/java/dev/alejo/habix/habits/data/local/HomeDatabase.kt
@@ -4,10 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import dev.alejo.habix.habits.data.local.entity.HabitEntity
+import dev.alejo.habix.habits.data.local.entity.HabitSyncEntity
 import dev.alejo.habix.habits.data.local.typeconverter.HomeTypeConverter
 
 @Database(
-    entities = [HabitEntity::class],
+    entities = [HabitEntity::class, HabitSyncEntity::class],
     version = 1
 )
 @TypeConverters(HomeTypeConverter::class)
