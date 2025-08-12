921411dcbb548001e70e0c665dbf1fab3eb34eb1
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-11 21:17:36 -0500

    feat: inject `HiltWorkerFactory` into App

--- app/src/main/java/dev/alejo/habix/App.kt
+++ app/src/main/java/dev/alejo/habix/App.kt
@@ -1,7 +1,19 @@
 package dev.alejo.habix
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var hiltWorkerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setWorkerFactory(hiltWorkerFactory)
+            .build()
+}
