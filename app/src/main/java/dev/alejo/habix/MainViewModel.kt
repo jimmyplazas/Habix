46a3217d1c275cf34f0d05348336e1fdd55b5918
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-15 13:07:35 -0500

    feat: implement sign out in MainViewModel

--- app/src/main/java/dev/alejo/habix/MainViewModel.kt
+++ app/src/main/java/dev/alejo/habix/MainViewModel.kt
@@ -6,18 +6,24 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.alejo.habix.authentication.domain.usecase.GetUserIdUseCase
+import dev.alejo.habix.authentication.domain.usecase.SignOutUseCase
 import dev.alejo.habix.onboarding.domain.usecase.HasSeenOnboardingUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val hasSeenOnboardingUseCase: HasSeenOnboardingUseCase,
-    private val getUSerIdUseCase: GetUserIdUseCase
+    private val getUSerIdUseCase: GetUserIdUseCase,
+    private val signOutUseCase: SignOutUseCase
 ) : ViewModel() {
 
     var hasSeenOnboarding by mutableStateOf(hasSeenOnboardingUseCase())
         private set
     var isLoggedIn by mutableStateOf(getUSerIdUseCase() != null)
         private set
+
+    fun signOut() {
+        signOutUseCase()
+    }
 
 }
