bdea62eb991141ad11e4cecff4c85595b7306cd3
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-15 13:07:19 -0500

    fix: make LoginScreen scrollable when keyboard is visible

--- app/src/main/java/dev/alejo/habix/authentication/presentation/login/LoginScreen.kt
+++ app/src/main/java/dev/alejo/habix/authentication/presentation/login/LoginScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -37,7 +40,9 @@ fun LoginScreen(
     Box(modifier = modifier.fillMaxSize()) {
         LoginBackground()
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .imePadding(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.weight(3f))
