a729fecfbeef3d1ed44d62881c926ab2bba7a5d4
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-16 10:49:19 -0500

    refactor: change PasswordResult to enum class

--- app/src/main/java/dev/alejo/habix/authentication/domain/usecase/ValidatePasswordUseCase.kt
+++ app/src/main/java/dev/alejo/habix/authentication/domain/usecase/ValidatePasswordUseCase.kt
@@ -3,26 +3,29 @@ package dev.alejo.habix.authentication.domain.usecase
 class ValidatePasswordUseCase {
     operator fun invoke(password: String): PasswordResult {
         if (password.isBlank()) {
-            return PasswordResult.Invalid("The password can't be blank")
+            return PasswordResult.INVALID_BLANK
         }
         if (password.length < 8) {
-            return PasswordResult.Invalid("The password needs to consist of at least 8 characters")
+            return PasswordResult.INVALID_LENGTH
         }
 
         val containsLettersAndDigits = password.any { it.isDigit() } && password.any { it.isLetter() }
         if (!containsLettersAndDigits) {
-            return PasswordResult.Invalid("The password needs to contain at least one letter and digit")
+            return PasswordResult.INVALID_CHARACTERS
         }
 
         val containsUpperCase = password.any { it.isUpperCase() }
         if (!containsUpperCase) {
-            return PasswordResult.Invalid("The password needs to contain at least one uppercase letter")
+            return PasswordResult.INVALID_UPPERCASE
         }
-        return PasswordResult.Valid
+        return PasswordResult.VALID
     }
 }
 
-sealed class PasswordResult {
-    object Valid : PasswordResult()
-    data class Invalid(val message: String) : PasswordResult()
+enum class PasswordResult {
+    VALID,
+    INVALID_BLANK,
+    INVALID_LENGTH,
+    INVALID_CHARACTERS,
+    INVALID_UPPERCASE,
 }
