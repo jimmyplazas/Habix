ad0dae42755a6fec08295285a13aad52d63f34cb
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-19 12:25:36 -0500

    feat: implement getUserToken in SessionManager

--- app/src/main/java/dev/alejo/habix/authentication/data/session/SessionManagerImpl.kt
+++ app/src/main/java/dev/alejo/habix/authentication/data/session/SessionManagerImpl.kt
@@ -3,7 +3,10 @@ package dev.alejo.habix.authentication.data.session
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import dev.alejo.habix.core.session.SessionManager
+import kotlinx.coroutines.tasks.await
 
 class SessionManagerImpl : SessionManager {
-    override fun getUserId(): String? = Firebase.auth.currentUser?.uid
+    override fun getUserId(): String? = getCurrentUser()?.uid
+    override suspend fun getUserToken(): String? = getCurrentUser()?.getIdToken(false)?.await()?.token
+    private fun getCurrentUser() = Firebase.auth.currentUser
 }
