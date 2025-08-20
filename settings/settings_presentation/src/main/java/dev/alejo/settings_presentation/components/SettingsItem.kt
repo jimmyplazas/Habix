af8505f0da38c43df3f9e8709f10f9a1e1f27af8
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-20 11:59:54 -0500

    refactor: move SettingsItem component to settings_presentation module

--- settings/settings_presentation/src/main/java/dev/alejo/settings_presentation/components/SettingsItem.kt
+++ settings/settings_presentation/src/main/java/dev/alejo/settings_presentation/components/SettingsItem.kt
@@ -1,4 +1,4 @@
-package dev.alejo.habix.settings.presentation.components
+package dev.alejo.settings_presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
