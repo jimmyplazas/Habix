aac7a48cfbce889ac28ddd6cff98c672ff898132
Jimmyale3102 <jimmyale3102@gmail.com>
2025-08-20 11:59:45 -0500

    refactor: update SettingsScreen import path

--- app/src/main/java/dev/alejo/habix/navigation/NavigationHost.kt
+++ app/src/main/java/dev/alejo/habix/navigation/NavigationHost.kt
@@ -14,7 +14,7 @@ import dev.alejo.habix.authentication.presentation.signup.SignUpScreen
 import dev.alejo.habix.habits.presentation.detail.DetailScreen
 import dev.alejo.habix.habits.presentation.home.HomeScreen
 import dev.alejo.habix.onboarding.presentation.OnboardingScreen
-import dev.alejo.habix.settings.presentation.SettingsScreen
+import dev.alejo.settings_presentation.SettingsScreen
 
 @Composable
 fun NavigationHost(
